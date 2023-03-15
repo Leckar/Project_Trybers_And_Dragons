6ca07a6244f848a20e8d2cb89c1cab25c967e395
Leckar <gusleckar@gmail.com>
2023-03-15 14:04:46 -0300

    Requisite 2 complete

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,11 +1,8 @@
 export default abstract class Race {
-  private static _instances = 0;
   constructor(
     private _name: string,
     private _dexterity: number,
-  ) {
-    Race._instances += 1;
-  }
+  ) { }
 
   get name(): string {
     return this._name;
